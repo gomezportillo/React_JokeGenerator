70571d3c24d6b2bc3c772306707b1625b3b7ea6f
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-07-11 10:32:36 +0200

    Displaying jokes using components with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react"
 
+import Joke from "./Joke"
+
 function App()
 {
   return (
     <div>
-      Hola
+      <Joke
+        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+        />
+
+      <Joke
+        question="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!"
+        />
+
+      <Joke
+        question="Did you hear about the mathematician who's afraid of negative numbers?"
+        punchline="He'll stop at nothing to avoid them!"
+        />
+
+      <Joke
+        question="Hear about the new restaurant called Karma?"
+        punchline="There’s no menu: You get what you deserve."
+        />
+
+      <Joke
+        question="Did you hear about the actor who fell through the floorboards?"
+        punchline="He was just going through a stage."
+        />
+
+      <Joke
+        question="Did you hear about the claustrophobic astronaut?"
+        punchline="He just needed a little space."
+        />
+
     </div>
   )
 }
---
+++ src/Joke.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Joke(props)
+{
+  return (
+    <div>
+      <h3 style={{display: props.question ? "block" : "none"}}>Question: {props.question}</h3>
+      <h3 style={{color: !props.question && "#999" }}>Answer: {props.punchline}</h3>
+      <hr/>
+    </div>
+  )
+}
+
+export default Joke
